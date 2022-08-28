18a5163fe6b1c20f2df5a801146dc6eff26f6aa5
Pavlo <pasam9300@gmail.com>
2022-08-28 18:09:36 +0200

    Update Submit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,20 +8,21 @@ const refs = {
 const STORAGE_KEY = 'feedback-form-state';
 
 refs.form.addEventListener('submit', onFormSubmit);
-
 refs.form.addEventListener('input', throttle(onImputValue, 500));
 
 function onFormSubmit(e) {
   e.preventDefault();
 
-  e.target.reset();
+  e.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
   console.log(formData);
 }
 
-const formData = {};
+let formData = {};
 
 function onImputValue(e) {
+  formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
   formData[e.target.name] = e.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
